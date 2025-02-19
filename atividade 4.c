712885f8d5135ff342004cfedceb3dda45515daa
bf05 <brunab.bf69@gmail.com>
2025-02-18 22:14:08 -0300

    Add files via upload

---
+++ atividade 4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 1;
+  printf("Letra: %c\n", proxima);
+   proxima = letra + 2;
+  printf("Letra: %c\n", proxima);
+    proxima = letra + 3;
+  printf("Letra: %c\n", proxima);
+    proxima = letra + 4;
+  printf("Letra: %c\n", proxima);
+    proxima = letra + 5;
+     printf("Letra: %c\n", proxima);
+    proxima = letra + 6;
+     printf("Letra: %c\n", proxima);
+    proxima = letra + 7;
+     printf("Letra: %c\n", proxima);
+    proxima = letra + 8;
+     printf("Letra: %c\n", proxima);
+    proxima = letra + 9;
+     printf("Letra: %c\n", proxima);
+    proxima = letra + 10;
+  return 0;
+}  
