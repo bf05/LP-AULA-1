3d5fa7b5962ab6eceeea43a36aed2af3beeedce4
bf05 <brunab.bf69@gmail.com>
2025-02-18 22:07:41 -0300

    Add files via upload

---
+++ atividade 3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  float usd = 5.68;
+  float euro= 5.94;
+  float btc = 534202.50;
+  printf("Cotação:%.3f", usd);
+  printf("cotação:%.3f , euro");
+  printf("cotação:%.3f, btc");
+  return 0;
+}  
